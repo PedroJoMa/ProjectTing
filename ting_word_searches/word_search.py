cc71a42895068c29e0147501919f1731d522e9e5
PedroJoMa <pejomadejefi@gmail.com>
2023-06-07 18:56:04 -0300

    feat: função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    response = []
+    for item in range(len(instance)):
+        format_data = {
+            "palavra": word,
+            "arquivo": instance.search(item)["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for line_number, line in enumerate(instance.search(item)
+                                           ["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                format_data["ocorrencias"].append({"linha": line_number + 1})
+
+        if format_data["ocorrencias"]:
+            response.append(format_data)
+
+    return response
 
 
 def search_by_word(word, instance):

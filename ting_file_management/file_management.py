7403235a7f9e1f38aa2ba5d1e165623971efb91a
PedroJoMa <pejomadejefi@gmail.com>
2023-06-06 23:30:53 -0300

    feat: função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aquii irá sua implementação"""
+    if not os.path.isfile(path_file):
+        error_message = f"Arquivo {path_file} não encontrado"
+        print(error_message, file=sys.stderr)
+        return []
+
+    if not path_file.lower().endswith(".txt"):
+        error_message = "Formato inválido"
+        print(error_message, file=sys.stderr)
+        return []
+
+    with open(path_file, "r") as file:
+        lines = file.read().split("\n")
+        return lines

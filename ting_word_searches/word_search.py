66f1dd20bb707f0199cc086bd43bac65d5f23ab7
PedroJoMa <pejomadejefi@gmail.com>
2023-06-07 20:20:58 -0300

    feat: search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -19,4 +19,23 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    response = []
+    for item in range(len(instance)):
+        format_data = {
+            "palavra": word,
+            "arquivo": instance.search(item)["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for line_number, line in enumerate(instance.search(item)
+                                           ["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                format_data["ocorrencias"].append({
+                    "linha": line_number + 1,
+                    "conteudo": line
+                })
+
+        if format_data["ocorrencias"]:
+            response.append(format_data)
+
+    return response
